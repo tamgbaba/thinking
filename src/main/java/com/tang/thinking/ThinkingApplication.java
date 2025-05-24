f381deb178de6d594efcae44b5fb7ee08a497182
tangzhongke <tangzk@dreamdt.cn>
2025-05-24 18:46:54 +0800

    项目更正为墨竹

--- src/main/java/com/tang/thinking/ThinkingApplication.java
+++ src/main/java/com/tang/thinking/ThinkingApplication.java
@@ -2,12 +2,19 @@ package com.tang.thinking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ThinkingApplication {
 
     public static void main(String[] args) {
             SpringApplication.run(ThinkingApplication.class, args);
     }
 
+    @RequestMapping("/")
+    public String thinking(){
+        return "墨竹出品必属精品";
+    }
 }
